9ee93fee0988f156034c7f9928771d68b36ef618
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-05-28 10:25:51 +0900

    refactor: Refactor request header creation logic

--- app/util.ts
+++ app/util.ts
@@ -1,72 +1,48 @@
 import Axios from 'axios'
-import Device from 'expo-device'
 import { Platform } from 'react-native'
-import Application from 'expo-application'
+import * as Device from 'expo-device'
+import * as Application from 'expo-application'
 import { tokenManager } from '../utils/localStorage'
 
 export const api = Axios.create({
-  baseURL: 'http://192.168.0.2:3000'
+  baseURL: 'http://192.168.0.2:3000',
 })
 
 const getDeviceId = async () => {
   if (Platform.OS === 'android') {
-    const deviceId = await Application?.getAndroidId()
+    const deviceId = await Application.getAndroidId()
     return deviceId
   } else {
-    const deviceId = await Application?.getIosIdForVendorAsync()
+    const deviceId = await Application.getIosIdForVendorAsync()
     return deviceId
   }
 }
 
-export const fetcher = async (url: string) => {
+const getHeaders = async () => {
   const Authorization = await tokenManager.getToken()
   const deviceId = await getDeviceId()
-  // const appCheckTokenResponse = await getToken(
-  //   appCheck,
-  //   /* forceRefresh= */ false
-  // )
-  try {
-    const { data } = await api.get(url, {
-      headers: {
-        Authorization: `Bearer ${Authorization}`,
-        'X-Device-id': deviceId,
-        'X-Platform': Platform.OS,
-        'X-Device': await Device.deviceName,
-        // 'X-Firebase-AppCheck': appCheckTokenResponse.token,
-      },
-    })
+  return {
+    Authorization: `Bearer ${Authorization}`,
+    'X-Device-id': deviceId,
+    'X-Platform': Platform.OS,
+    'X-Device': await Device.deviceName,
+  }
+}
 
+export const fetcher = async (url: string) => {
+  try {
+    const { data } = await api.get(url, { headers: await getHeaders() })
     return data?.body
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     return error.response?.data
   }
 }
 
 export const poster = async (url: string, data: object) => {
-  const Authorization =  await tokenManager.getToken()
-  const deviceId = await getDeviceId()
-  // const appCheckTokenResponse = await getToken(
-  //   appCheck,
-  //   /* forceRefresh= */ true
-  // )
   try {
-    const response = await api.post(url, data, {
-      headers: {
-        Authorization: `Bearer ${Authorization}`,
-        'X-Device-ID': deviceId,
-        'X-Platform': Platform.OS,
-        'X-Device': await Device.deviceName,
-        // 'X-Firebase-AppCheck': appCheckTokenResponse.token,
-      },
-    })
-
+    const response = await api.post(url, data, { headers: await getHeaders() })
     return response.data
   } catch (error: any) {
     return error.response?.data
   }
 }
-
-// export const apiUrl = (path: string) =>
-//   `${import.meta.env.VITE_API_BASE}${path}`
-
