d6e025b910b781a00ae47c96405729531fcea5b5
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-07-02 09:51:37 +0900

    chore: Update push notification registration process

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,9 @@
 import { config } from '@gluestack-ui/config'
 import { GluestackUIProvider } from '@gluestack-ui/themed'
-import * as Notifications from 'expo-notifications'
 import { Stack } from 'expo-router/stack'
 import React from 'react'
-import { Platform } from 'react-native'
-import { AlertRef } from '../components/alert'
-import { poster } from './util'
 
 export default function Layout() {
-  registerToken()
   return (
     <GluestackUIProvider config={config}>
       <Stack>
@@ -21,44 +16,4 @@ export default function Layout() {
       </Stack>
     </GluestackUIProvider>
   )
-}
-
-async function registerToken() {
-  const token = await registerForPushNotificationsAsync()
-  console.log('token', token)
-  await poster('/auth/notification', { token })
-}
-
-async function registerForPushNotificationsAsync() {
-  const alertRef = React.useRef<AlertRef>(null)
-  let token
-  const { status: existingStatus } = await Notifications.getPermissionsAsync()
-  let finalStatus = existingStatus
-  if (existingStatus !== 'granted') {
-    const { status } = await Notifications.requestPermissionsAsync()
-    finalStatus = status
-  }
-  if (finalStatus !== 'granted') {
-    return alert('Failed to get push token for push notification!')
-  }
-  token = (await Notifications.getExpoPushTokenAsync()).data
-
-  if (Platform.OS === 'android') {
-    Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    })
-  } else if (Platform.OS === 'ios') {
-    Notifications.setNotificationHandler({
-      handleNotification: async () => ({
-        shouldShowAlert: true,
-        shouldPlaySound: true,
-        shouldSetBadge: true,
-      }),
-    })
-  }
-
-  return token
 }
--- app/tabs/_layout.tsx
+++ app/tabs/_layout.tsx
@@ -1,8 +1,13 @@
-import { Tabs } from 'expo-router'
 import FontAwesome from '@expo/vector-icons/FontAwesome'
+import * as Notifications from 'expo-notifications'
+import { Tabs } from 'expo-router'
+import React from 'react'
+import { Platform } from 'react-native'
+import { AlertRef } from '../../components/alert'
+import { poster } from '../util'
 
 export default function TabLayout() {
-
+  registerToken()
   return (
     <Tabs screenOptions={{ tabBarActiveTintColor: '#036B3F' }}>
       <Tabs.Screen
@@ -37,4 +42,44 @@ export default function TabLayout() {
       />
     </Tabs>
   )
+}
+
+async function registerToken() {
+  const token = await registerForPushNotificationsAsync()
+  console.log('token', token)
+  await poster('/auth/notification', { token })
+}
+
+async function registerForPushNotificationsAsync() {
+  const alertRef = React.useRef<AlertRef>(null)
+  let token
+  const { status: existingStatus } = await Notifications.getPermissionsAsync()
+  let finalStatus = existingStatus
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync()
+    finalStatus = status
+  }
+  if (finalStatus !== 'granted') {
+    return alert('Failed to get push token for push notification!')
+  }
+  token = (await Notifications.getExpoPushTokenAsync()).data
+
+  if (Platform.OS === 'android') {
+    Notifications.setNotificationChannelAsync('default', {
+      name: 'default',
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: '#FF231F7C',
+    })
+  } else if (Platform.OS === 'ios') {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+      }),
+    })
+  }
+
+  return token
 }
