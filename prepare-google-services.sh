e366f02425b8fe7bb11bf464529df095cbc7a7c0
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-07-19 21:46:35 +0900

    chore: Update app configuration file

--- prepare-google-services.sh
+++ prepare-google-services.sh
@@ -6,3 +6,5 @@ mkdir -p ./googleServices
 # Decode the base64 strings and create the files
 echo $GOOGLE_SERVICES_JSON_BASE64 | base64 --decode > ./googleServices/google-services.json
 echo $GOOGLE_SERVICES_PLIST_BASE64 | base64 --decode > ./googleServices/GoogleService-Info.plist
+
+echo "Google Services files created successfully"
