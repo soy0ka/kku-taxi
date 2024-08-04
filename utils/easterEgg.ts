3ddb666253625da10d172d68271e49d3ee1cc8ef
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-04 19:52:01 +0900

    chore: update easteregg meassge

--- utils/easterEgg.ts
+++ utils/easterEgg.ts
@@ -13,19 +13,19 @@ const ArrivalAndDestinationsAreSame = (count: number) => {
     case 5:
       return '대체 얼마나 누르시는걷에요?'
     case 6:
-      return '하.. 알겠습니다 여기까지 오실지는 몰랐어요'
+      return '알겠습니다 여기까지 오실지는 몰랐어요'
     case 7:
       return '아니 이제 그만 누르세요'
     case 8:
-      return '언제까지 누르실거에요...'
+      return '언제까지 누르실거에요..?'
     case 9:
-      return '하 해보자는건가요?'
+      return '한번 해보자는 건가요?'
     case 10:
       return '제가 얼마나 만들어놓았는지 궁굼하시다고요?'
     case 11:
-      return '하 한번 더 누르면 뭔가 일어날거에요'
+      return '한번만 더 누르시기만 해봐요!'
     case 12:
-      return '그만하라고 했어요 또 누르면 계정 삭제해버릴거에요'
+      return '전 그만하라고 했어요 다음에도 누르면 계정을 삭제해버릴거에요'
     case 13:
       return '3'
     case 14:
@@ -43,7 +43,7 @@ const ArrivalAndDestinationsAreSame = (count: number) => {
     case 20:
       return '....'
     case 21:
-      return '계정이 삭제되었습니다'
+      return '사용자의 계정이 삭제되었습니다 \n 이용해주셔서 감사합니다'
     case 22:
       return '겠냐고요...'
     case 23:
