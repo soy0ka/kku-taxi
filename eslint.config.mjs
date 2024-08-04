116dddde50c5fcf665a050f926ef707b72076c8b
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-04 19:33:02 +0900

    style: update eslint.config.mjs to adhere to eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,11 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
-
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
-  {languageOptions: { globals: globals.browser }},
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
-];
+]
