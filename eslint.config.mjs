a4a5448ada11b877426ca6099df817d884783527
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-05 15:19:05 +0900

    feat: configure eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,24 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReactConfig,
+  {
+    settings: {
+      react: {
+        version: 'detect', // 또는 '17.0', '16.13', 등 명시적인 버전
+      },
+    },
+  },
 ]
