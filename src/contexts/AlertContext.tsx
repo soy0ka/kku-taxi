d64f788105c5eb8942dc8f14a6fbeed1b7bc3bb0
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-09 16:19:37 +0900

    feat: made AlertContext to use showAlert function globally

---
+++ src/contexts/AlertContext.tsx
@@ -0,0 +1,35 @@
+import { Alert, AlertRef } from '@/components/alert'
+import React, { createContext, ReactNode, useContext, useRef } from 'react'
+
+type AlertContextType = {
+  showAlert: (alertTitle: string, alertMessage: string) => void
+}
+
+const AlertContext = createContext<AlertContextType | undefined>(undefined)
+
+interface AlertProviderProps {
+  children: ReactNode
+}
+
+export const AlertProvider: React.FC<AlertProviderProps> = ({ children }) => {
+  const alertRef = useRef<AlertRef>(null)
+
+  const showAlert = (alertTitle: string, alertMessage: string) => {
+    alertRef.current?.openAlert(alertTitle, alertMessage)
+  }
+
+  return (
+    <AlertContext.Provider value={{ showAlert }}>
+      {children}
+      <Alert ref={alertRef} />
+    </AlertContext.Provider>
+  )
+}
+
+export const useAlert = () => {
+  const context = useContext(AlertContext)
+  if (!context) {
+    throw new Error('useAlert must be used within an AlertProvider')
+  }
+  return context
+}
