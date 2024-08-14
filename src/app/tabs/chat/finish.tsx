18a0169ad6fea59c5e6fde643fc8b2c199700902
soyoka <55011525+soy0ka@users.noreply.github.com>
2024-08-14 16:50:07 +0900

    refactor: Update Finish component to include feedback submission functionality

--- src/app/tabs/chat/finish.tsx
+++ src/app/tabs/chat/finish.tsx
@@ -1,5 +1,7 @@
 import { FeedbackAccordion } from '@/components/chat/feedbackAccordion'
-import { fetcher } from '@/utils/apiClient'
+import { useAlert } from '@/contexts/AlertContext'
+import { ApiStatus } from '@/types/api'
+import { fetcher, poster } from '@/utils/apiClient'
 import { Profile } from '@/utils/gravatar'
 import {
   Avatar,
@@ -26,10 +28,18 @@ interface PartyMember {
     email: string
   }
 }
+
+interface Issue {
+  userId: number
+  issue: string[]
+}
+
 export default function Finish() {
   const { id } = useLocalSearchParams()
   const [members, setMembers] = React.useState<PartyMember[]>([])
+  const [issues, setIssues] = React.useState<Issue[]>([])
   const navigation = useNavigation()
+  const { showAlert } = useAlert()
 
   React.useEffect(() => {
     const unsubscribe = navigation.addListener('focus', () => {
@@ -39,11 +49,34 @@ export default function Finish() {
     return unsubscribe
   }, [navigation])
 
+  const handleSubmission = async () => {
+    const response = await poster(`/party/${id}/finish`, {
+      feedBack: issues
+    })
+
+    if (response.status === ApiStatus.SUCCESS) {
+      showAlert('운행 종료', '피드백이 제출되었습니다')
+    } else {
+      showAlert('운행 종료', '피드백 제출에 실패했습니다')
+    }
+  }
+
   const fetchMembers = async () => {
-    const members = await fetcher(`/party/${id}/membsers`)
+    const members = await fetcher(`/party/${id}/members`)
     console.log(members)
     setMembers(members.data)
   }
+
+  const handleIssueChange = (userId: number, newIssues: string[]) => {
+    setIssues((prevIssues) => {
+      // 기존의 이슈를 업데이트하거나 새로 추가
+      const updatedIssues = prevIssues.filter(
+        (issue) => issue.userId !== userId
+      )
+      return [...updatedIssues, { userId, issue: newIssues }]
+    })
+  }
+
   return (
     <SafeAreaView sx={{ flex: 1 }}>
       <ScrollView style={{ padding: 5 }} mt={10}>
@@ -85,7 +118,15 @@ export default function Finish() {
                         @{member.User.email.split('@')[0]}
                       </Text>
                     </HStack>
-                    <FeedbackAccordion />
+                    <FeedbackAccordion
+                      selectedIssues={
+                        issues.find((issue) => issue.userId === member.User.id)
+                          ?.issue || []
+                      }
+                      onChange={(newIssues) =>
+                        handleIssueChange(member.User.id, newIssues)
+                      }
+                    />
                   </VStack>
                 </Card>
               ))}
@@ -104,7 +145,7 @@ export default function Finish() {
           action="positive"
           sx={{ width: '100%' }}
           borderWidth="$0"
-          onPress={() => {}}
+          onPress={handleSubmission}
         >
           <ButtonText>제출하기</ButtonText>
         </Button>
--- src/components/chat/feedbackAccordion.tsx
+++ src/components/chat/feedbackAccordion.tsx
@@ -16,7 +16,36 @@ import {
 } from '@gluestack-ui/themed'
 import React from 'react'
 
-export const FeedbackAccordion: React.FC = () => {
+interface Issue {
+  label: string
+  value: string
+}
+interface FeedbackAccordionProps {
+  selectedIssues: string[] // 선택된 문제 항목들의 값 배열
+  // eslint-disable-next-line no-unused-vars
+  onChange: (selected: string[]) => void // 선택 항목이 변경될 때 호출되는 콜백
+}
+
+export const FeedbackAccordion: React.FC<FeedbackAccordionProps> = ({
+  selectedIssues = [],
+  onChange = () => {}
+}) => {
+  const handleCheckboxChange = (issueValue: string) => {
+    if (selectedIssues.includes(issueValue)) {
+      onChange(selectedIssues.filter((selected) => selected !== issueValue))
+    } else {
+      onChange([...selectedIssues, issueValue])
+    }
+  }
+
+  const issues: Issue[] = [
+    { label: '미터기 요금과 다른 금액을 요구했어요', value: 'DIFFERENT_FARE' },
+    { label: '장소에 나타나지 않았어요 (노쇼)', value: 'NO_SHOW' },
+    { label: '제 시간에 도착하지 않았어요 (지각)', value: 'LATE' },
+    { label: '정산 금액을 보내지 않았어요', value: 'NO_PAYMENT' },
+    { label: '비매너 행동을 했어요', value: 'BAD_MANNER' }
+  ]
+
   return (
     <Accordion
       width="100%"
@@ -44,25 +73,20 @@ export const FeedbackAccordion: React.FC = () => {
           </AccordionTrigger>
         </AccordionHeader>
         <AccordionContent>
-          {[
-            '미터기 요금과 다른 금액을 요구했어요',
-            '아예 나타나지 않았어요 (노쇼)',
-            '제 시간에 도착하지 않았어요 (지각)',
-            '정산 금액을 보내지 않았어요',
-            '장소에 나타나지 않았어요',
-            '비매너 행동을 했어요'
-          ].map((value) => (
+          {issues.map((issue) => (
             <Checkbox
               size="md"
               mt="$0.5"
               aria-label="Checkbox"
-              value={''}
-              key={value}
+              value={issue.value}
+              key={issue.value}
+              isChecked={selectedIssues.includes(issue.value)}
+              onChange={() => handleCheckboxChange(issue.value)}
             >
               <CheckboxIndicator mr="$2">
                 <CheckboxIcon as={CheckIcon} />
               </CheckboxIndicator>
-              <CheckboxLabel>{value}</CheckboxLabel>
+              <CheckboxLabel>{issue.label}</CheckboxLabel>
             </Checkbox>
           ))}
         </AccordionContent>
